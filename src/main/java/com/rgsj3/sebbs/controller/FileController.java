416152e8d738a9605b30d48b0a64509da82db800
艾莉舞露 <605717765@qq.com>
2019-05-26 17:21:06 +0800

    下载文件

--- src/main/java/com/rgsj3/sebbs/controller/FileController.java
+++ src/main/java/com/rgsj3/sebbs/controller/FileController.java
@@ -2,6 +2,7 @@ package com.rgsj3.sebbs.controller;
 
 import com.rgsj3.sebbs.domain.Result;
 import com.rgsj3.sebbs.service.FileService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 public class FileController {
@@ -20,5 +23,11 @@ public class FileController {
     public Result addFile(@RequestParam("file") MultipartFile multipartFile,
                           HttpServletRequest httpServletRequest){
         return fileService.addFile(multipartFile, httpServletRequest);
+    }
+
+    @RequestMapping("download/{id}")
+    public String downLoadFile(HttpServletResponse response,
+                           @PathVariable("id") Integer id) throws UnsupportedEncodingException {
+        return fileService.downloadFile(response, id);
     }
 }
--- src/main/java/com/rgsj3/sebbs/service/FileService.java
+++ src/main/java/com/rgsj3/sebbs/service/FileService.java
@@ -10,7 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.List;
 import org.slf4j.Logger;
@@ -58,5 +60,44 @@ public class FileService {
             }
             return Result.error(5, "上传失败");
         }
+    }
+
+    public String downloadFile(HttpServletResponse response, Integer id) throws UnsupportedEncodingException {
+        var file = fileRepository.findById(id).get();
+        String fileName= file.getName();
+        java.io.File dest = new java.io.File(file.getPath());
+        if(dest.exists()){ //判断文件父目录是否存在
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + URLEncoder.encode(fileName, "UTF-8"));
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(dest);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("----------file download" + fileName);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
