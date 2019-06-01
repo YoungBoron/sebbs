2f08ae5d126bac05f0f96f83e1b977c717b07782
艾莉舞露 <605717765@qq.com>
2019-06-01 16:16:48 +0800

    Revert "Revert "Update course.js""
    This reverts commit a5fbcf33b7c3f87df8ad6642024094cfdbc6e3dc.

--- src/main/resources/static/course.js
+++ src/main/resources/static/course.js
@@ -1,5 +1,8 @@
 $(".setScore").click(function() {
-    $.post("/set/score", new FormData($(this).parent()), function(data) {
+    $.post("/set/score", {
+        id: $(this).siblings("input[name='id']").val(),
+        score: $(this).siblings("input[name='score']").val()
+    }, function(data) {
         //alert(data.code);
         if (data.code == 0) {
             alert("修改成功");
