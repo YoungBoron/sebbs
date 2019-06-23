3dc0b340ba0bcde26d81d21d1d7b62383a1e7925
艾莉舞露 <605717765@qq.com>
2019-06-23 14:13:20 +0800

    评分验证

--- src/main/java/com/rgsj3/sebbs/domain/StudentCourse.java
+++ src/main/java/com/rgsj3/sebbs/domain/StudentCourse.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @Entity
 public class StudentCourse {
@@ -11,6 +14,8 @@ public class StudentCourse {
     @GeneratedValue
     private Integer id;
 
+    @Max(value = 100, message = "分数必须小于等于100")
+    @Min(value = 0, message = "分数必须大于等于0")
     private Double score;
 
     @ManyToOne(targetEntity = Course.class)
